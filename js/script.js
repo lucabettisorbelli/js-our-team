75774687e27c71adbf7f16d9a7cbcdbe11ba111c
luca <Luca betti sorbelli>
2023-05-02 16:42:37 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -9,23 +9,44 @@ const membri = [
     { nome:"Barbara Ramos",	ruolo: "Graphic Designer",	foto: "barbara-ramos-graphic-designer.jpg"}
 ]
 
-// console.log(membri);
-
-
-
 //console.log proprietà singole
-
 
 const container = document.querySelector(".container");
 
-for (let i = 0; i < membri.length; i++) {
+// for (let i = 0; i < membri.length; i++) {
 
+//     let membro = membri[i];    
+
+//     const membroDom = document.createElement("p");
+//     membroDom.innerText = `Nome: ${membro.nome}, Ruolo: ${membro.ruolo}, Foto: ${membro.foto}`;
+//     container.appendChild(membroDom);
+//     membroDom.style.textAlign = "center";
+// }
+
+
+
+
+for (let i = 0; i < membri.length; i++) {
+    
+    
     let membro = membri[i];
     
     console.log(`Nome: ${membro.nome}, Ruolo: ${membro.ruolo}, Foto: ${membro.foto}`);
 
-    const membroDom = document.createElement("p");
-    membroDom.innerText = `Nome: ${membro.nome}, Ruolo: ${membro.ruolo}, Foto: ${membro.foto}`;
-    container.appendChild(membroDom);
-    membroDom.style.textAlign = "center";
+    // crea un div per ogni proprietà
+    const membroDiv = document.createElement("div");
+
+    const nomeMembro = document.createElement("p");
+    nomeMembro.textContent = `Nome: ${membro.nome}`;
+    membroDiv.appendChild(nomeMembro);
+
+    const ruoloMembro = document.createElement("p");
+    ruoloMembro.textContent = `Ruolo: ${membro.ruolo}`;
+    membroDiv.appendChild(ruoloMembro);
+
+    const fotoImg = document.createElement("img");
+    fotoImg.src = `img/${membro.foto}`;
+    membroDiv.appendChild(fotoImg);
+
+    container.appendChild(membroDiv);
 }
